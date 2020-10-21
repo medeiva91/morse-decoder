78427cfe81469aedbfa134543403163806f5af02
WIN-JR9EEP9V1OJ\medeiva <anastasia.pryakhina@breffi.ru>
2020-10-21 17:11:56 +0200

    feat: add desion

--- src/index.js
+++ src/index.js
@@ -38,7 +38,42 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  const size = expr.length-1;
+  console.log(expr.length);
+
+  let count = 0;
+  let letter = '';
+  let sentence ='';
+
+  while (count < size) {
+
+    console.log("count", count);
+
+    if(expr[count]!="*"){
+
+      let letterChar = '';
+
+      for (let i=count; i<count+10; i+=2) {
+
+        exprChar = expr.slice(i, i+2);
+        if (exprChar == '10') {
+          letterChar += '.';
+        } else if (exprChar == '11') {
+          letterChar += '-'
+        } else if (exprChar == '00') {
+          letterChar = '';
+        }
+
+      }
+      sentence +=  MORSE_TABLE[letterChar];
+      count += 10;
+    } else {
+      sentence += " ";
+      count+=10;
+    }
+  }
+
+  return sentence;
 }
 
 module.exports = {
